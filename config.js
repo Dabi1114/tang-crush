3bb004a28e5e9f51ccb5177935c3d2d3248f1aab
Dabi1114 <106763502+Dabi1114@users.noreply.github.com>
2022-06-02 23:49:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
     question:'Đồng ý rồi đó nha, tới chừng đó không được từ chối đâu ă',
     btnReply: 'Okiii a :v',
     reply: 'Chúc mừng sinh nhật',
-    mess: 'À quên chúc e đậu tốt nghiệp và đậu vào trường e chọn :v',
+    mess: 'À quên chúc e đậu tốt nghiệp và đậu vào trường e chọn nha :v',
     messDesc: 'Cố lên nha e, còn hơn tháng nữa là khỏe à.',
     btnAccept: 'HAPPY BIRTHDAY',
     messLink: 'https://www.facebook.com/profile.php?id=100054503302062
